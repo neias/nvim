2498112fd16e470dd07eb3bd8334c36324b07bed
neias <emreacar@hotmail.com>
2024-11-18 12:02:55 +0300

    ---

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -3,10 +3,11 @@ local M = {}
 function M.toggle_theme_mode()
 	if vim.o.background == "dark" then
 		vim.o.background = "light"
-		vim.cmd("colorscheme github_light")
+		vim.cmd("colorscheme tokyonight-day")
+		vim.cmd([[highlight Normal guibg=#ffffff guifg=#000000]])
 	else
 		vim.o.background = "dark"
-		vim.cmd("colorscheme github_dark")
+		vim.cmd("colorscheme solarized-osaka")
 	end
 end
 
