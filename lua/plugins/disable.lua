112f116efd74b82ebe45df287aec3a922b6d5192
Emre Acar <emreacar@192.168.1.13>
2025-10-02 12:36:23 +0300

    yeni versiyon güncelleme

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,8 +1,2 @@
 -- LazyVim'in varsayılan plugin'lerini devre dışı bırakmak için
-return {
-	-- Gitsigns'ı Windows'ta devre dışı bırak
-	{
-		"lewis6991/gitsigns.nvim",
-		enabled = false,
-	},
-}
+return {}
